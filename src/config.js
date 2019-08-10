eda2b7dafbc7ad305299ba0198eda53c4ae02077
Hali Power <hpower0809@gmail.com>
2019-08-10 14:02:54 +0000

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/noteful/api'
+  API_ENDPOINT: 'https://boiling-fortress-96005.herokuapp.com/noteful/api',
+  API_KEY: process.env.REACT_APP_API_KEY
 }
